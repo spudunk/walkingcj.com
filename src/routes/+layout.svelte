ef8d54683d1b15f2b8f92f7f82699744273c1f11
embed <brydon@helium.email>
2023-08-15 17:51:10 -0400

    refactor: initialize svelte project
    Created a new SvelteKit project with TailwindCSS, unplugin-icons, OPG & Twitter metadata and Poppins font.

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,40 @@
+<script lang="ts">
+    import '../app.css';
+</script>
+
+<svelte:head>
+    <title>Walking CJ Ranch</title>
+    
+    <!-- base metadata -->
+    <meta
+      name="description"
+      content="High quality beef in SW Washington State. We believe that a quality life makes quality meat Contact us today to join our network or waiting list."
+    />
+
+    <!-- open graph / facebook metadata -->
+    <meta property="og:url" content="https://walkingcj.com" />
+    <meta property="og:type" content="website" />
+    <meta property="og:title" content="Walking CJ Ranch" />
+    <meta property="og:description" content="High quality beef in SW Washington State. We believe that a quality life makes quality meat Contact us today to join our network or waiting list." />
+    <meta property="og:image" content="https://walkingcj.com/og_image.jpeg" />
+
+    <!-- twitter / x metadata -->
+    <meta name="twitter:card" content="summary_large_image" />
+    <meta property="twitter:domain" content="walkingcj.com" />
+    <meta property="twitter:url" content="https://walkingcj.com" />
+    <meta name="twitter:title" content="Walking CJ Ranch" />
+    <meta name="twitter:description" content="High quality beef in SW Washington State. We believe that a quality life makes quality meat Contact us today to join our network or waiting list." />
+    <meta name="twitter:image" content="https://walkingcj.com/og_image.jpeg" />
+
+    <!-- google fonts -->
+    <link rel="preconnect" href="https://fonts.googleapis.com" />
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin="true" />
+    <link
+      href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;700&display=swap"
+      rel="stylesheet"
+    />
+
+
+</svelte:head>
+
+<slot />
