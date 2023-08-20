8e0399e843cab4681628ffe31362d742298ccec7
embed <brydon@helium.email>
2023-08-20 03:52:47 -0400

    feat: product section

---
+++ src/lib/components/ProductCard.svelte
@@ -0,0 +1,61 @@
+<script lang="ts">
+	export let img: string = '';
+	export let alt: string = '';
+	export let title: string = '';
+	export let subtitle: string | undefined;
+	export let description: string = '';
+	export let longDescription: string = '';
+	export let icon: string = '';
+
+	import LucideBeef from '~icons/lucide/beef';
+	import GisLayerLandcover from '~icons/gis/layer-landcover';
+</script>
+
+<div
+	class="zoombg relative h-96 w-full overflow-hidden text-white md:w-full"
+	style={`--url: url('${img}')`}
+>
+	<div
+		class="absolute left-0 top-0 z-40 flex h-full w-full flex-col items-center justify-center gap-2 rounded-lg bg-black bg-opacity-50 p-2 md:rounded-none"
+	>
+		<div class="flex flex-col items-center gap-1">
+			<h2 class="text-4xl">{title}</h2>
+			{#if subtitle}
+				<h3 class="text-2xl">{subtitle}</h3>
+			{/if}
+		</div>
+		<div class="flex max-w-md flex-col items-center gap-2">
+			<span class="text-md text-center text-sm">{description}</span>
+			<button class="flex h-full items-center gap-2 rounded-lg bg-red-700 px-2 py-2 text-lg">
+				{#if icon == 'beef'}
+					<LucideBeef />
+				{:else if icon == 'location'}
+					<GisLayerLandcover />
+				{/if}
+				Learn More
+			</button>
+		</div>
+	</div>
+</div>
+
+<style>
+	.zoombg::before {
+		content: '';
+		position: absolute;
+		/* padding-top: 100%; */
+		top: 0;
+		left: 0;
+		width: 100%;
+		height: 100%;
+		background-image: var(--url);
+		background-position: center;
+		background-size: cover;
+		transition: transform 0.3s ease;
+		transform-origin: center;
+		z-index: 39;
+	}
+
+	.zoombg:hover::before {
+		transform: scale(1.1);
+	}
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,31 @@
 <script lang="ts">
 	import Carousel from '$lib/components/Carousel.svelte';
+	import ProductCard from '$lib/components/ProductCard.svelte';
 	import FeMouse from '~icons/fe/mouse';
+	const products = [
+		{
+			img: '/images/products/0.webp',
+			alt: '',
+			title: 'High Quality Beef',
+			subtitle: 'Raised to Order',
+			icon: 'beef',
+			description:
+				"We'll raise your beef the right way with options on genetics and finishing. Call today for availability and to get on the waiting list.",
+			longDescription:
+				"We have several herds in the Lewis County area with different genetics and types of pasture. To maintain the highest quality we don't overpopulate our fields, so we limit our capacity. For this reason we keep a waiting list. Our customers know that the best is worth waiting for, and our quality doesn't disappoint. Occasionally an animal doesn't produce to our standards so we keep a small herd reserved to fill orders in these cases. We take our time raising, finishing, and aging our product to maintain a strict and refined process. We work on the timeline of the animal, not the customer. Trust us, the difference is clear in the results."
+		},
+		{
+			img: '/images/products/1.webp',
+			alt: '',
+			title: 'Pasture Leasing',
+			subtitle: undefined,
+			icon: 'location',
+			description:
+				'Lease your land to trustworthy and humane ranchers and oversee your animals throughout their life',
+			longDescription:
+				"We don't like to reveal our exact process but we are transparent about what makes the quality. If you have good acreage, want to lease it, and want to be part of our network we'll show you how to maximize the potential of your pasture. To maintain our quality we are selective in who we work with and strict in our process, but if you truly care about the life of the animals and maintaining the highest quality of food for your family then give us a call. Whether or not your land qualifies, we'd be happy to talk with you and love to help fellow small farmers."
+		}
+	];
 </script>
 
 <!-- HERO -->
@@ -15,3 +40,12 @@
 		<!-- <FeMouse class="absolute bottom-16 mx-auto animate-bounce text-5xl text-white" /> -->
 	</div>
 </div>
+	<!-- Products -->
+	<div class="flex flex-col gap-4 px-4 pt-4 text-center md:px-0">
+		<h2 class="text-4xl font-semibold">What we do</h2>
+		<div class="flex flex-col justify-center gap-9 md:flex-row md:gap-1">
+			{#each products as product}
+				<ProductCard {...product} />
+			{/each}
+		</div>
+	</div>
