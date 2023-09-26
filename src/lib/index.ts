d35112a6b59e7e0ddf8e946d5e52f748512d6e1f
Christopher Hicks <hickscme@gmail.com>
2023-09-26 08:09:10 -0700

    create single data object

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,1 +1,61 @@
 // place files you want to import through the `$lib` alias in this folder.
+
+export const data = {
+  google: "https://local.google.com/place?id=11550191565085024000&use=srp", 
+  // google: "https://www.google.com/search?q=Walking+CJ+Ranch&ludocid=11550191565085024000&localrlb=1&lsig=AB86z5XP9PDrDxOnMtEaJFDFuzWb",
+  facebook: "https://www.facebook.com/WalkingCJ",
+  maps: "https://www.google.com/maps/place/Walking+CJ+Ranch/data=!4m2!3m1!1s0x5493ff96dd06d21f:0xa04a86086c9b1f00?hl=en",
+  phoneLink: "tel:3602695572",
+  phone: "(360) 269-5572",
+  emailLink: "mailto:john@walkingcj.com",
+  
+}
+
+export const reviews = [
+  {
+    name: 'Sage B',
+    review:
+      '“Love them chops”\n“tenderloin was amazing”\n“lovin’ the t-bone”\n“Bacon is sooooo good”',
+    img: 'https://res.cloudinary.com/dt9pi5obp/image/upload/v1692845720/walkingcj/IMG_6180_hkfimq.heic',
+    alt: 'steak dinner',
+  },
+  {
+    name: 'Angel A',
+    review:
+      'Felt like a warm hug for my tastebuds.',
+    img: '',
+    alt: '',
+  },
+  {
+    name: 'Chris H',
+    review:
+      'It\'s not even worth getting beef at the grocery store anymore.',
+    img: '',
+    alt: '',
+  }
+];
+
+export const products = [
+  {
+    img: 'https://res.cloudinary.com/dt9pi5obp/image/upload/v1692807805/walkingcj/IMG_3091_n8dgg2.heic',
+    alt: '',
+    title: 'High Quality Beef',
+    subtitle: 'Raised to Order',
+    icon: 'beef',
+    description:
+      "We'll raise your beef the right way with options on genetics and finishing. Call today for availability and to get on the waiting list.",
+    longDescription:
+      "We have several herds in the Lewis County area with different genetics and types of pasture. To maintain the highest quality we don't overpopulate our fields, so we limit our capacity. For this reason we keep a waiting list. Our customers know that the best is worth waiting for, and our quality doesn't disappoint. Occasionally an animal doesn't produce to our standards so we keep a small herd reserved to fill orders in these cases. We take our time raising, finishing, and aging our product to maintain a strict and refined process. We work on the timeline of the animal, not the customer. Trust us, the difference is clear in the results."
+  },
+  {
+    img: 'https://res.cloudinary.com/dt9pi5obp/image/upload/v1692807807/walkingcj/IMG_3106_fhbjg9.heic',
+    alt: '',
+    title: 'Pasture Leasing',
+    subtitle: undefined,
+    icon: 'location',
+    description:
+      'Lease your land to trustworthy and humane ranchers and oversee your animals throughout their life',
+    longDescription:
+      "We don't like to reveal our exact process but we are transparent about what makes the quality. If you have good acreage, want to lease it, and want to be part of our network we'll show you how to maximize the potential of your pasture. To maintain our quality we are selective in who we work with and strict in our process, but if you truly care about the life of the animals and maintaining the highest quality of food for your family then give us a call. Whether or not your land qualifies, we'd be happy to talk with you and love to help fellow small farmers."
+  }
+];
